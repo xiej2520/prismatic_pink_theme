a8e04480e2d5bd6f4bb49d238ba142d4eb9cee68
xiej2520 <xiej2520@gmail.com>
2026-09-06 19:58:06 -0500

    fix CSS theme generation, update package scripts & deps

--- build/buildCssTheme.ts
+++ build/buildCssTheme.ts
@@ -25,24 +25,55 @@ function themeVars(color: UiColors, syntax: SyntaxColors, type: "light" | "dark"
     "--pp-bg": color.ui.primaryBg,
     "--pp-bg-alt": color.ui.secondaryBg,
     "--pp-bg-deep": color.ui.tertiaryBg,
+
     "--pp-fg": color.text.normal,
     "--pp-fg-muted": color.text.muted,
     "--pp-fg-light": color.text.light,
-    "--pp-comment": syntax.lightPink,
+    "--pp-fg-bold": color.text.bold,
+    "--pp-fg-inverse": color.text.inverse,
+    "--pp-fg-emphasized": color.text.emphasised,
+    "--pp-fg-faded": color.text.faded,
+    "--pp-placeholder": color.ui.placeholderText,
+    "--pp-disabled": color.ui.disabledText,
+
     "--pp-border": color.ui.border,
-    "--pp-selection": color.text.selectionBg,
+    "--pp-dropdown-bg": color.ui.dropdownBg,
+    "--pp-input-bg": color.ui.inputBg,
+    "--pp-drop-bg": color.ui.primaryDropBg,
+    "--pp-separator": color.ui.separator,
+
+    "--pp-hover-bg": color.ui.hoverBgA,
+    "--pp-active-bg": color.ui.activeBgA,
+    "--pp-list-hover-bg": color.ui.listHoverBgA,
+    "--pp-list-inactive-bg": color.ui.listInactiveBgA,
+    "--pp-selected-bg": color.ui.selectedBg,
+    "--pp-selected-secondary-bg": color.ui.selectedSecondaryBg,
+
+    "--pp-scrollbar-bg": color.ui.scrollBgA,
+    "--pp-scrollbar-hover-bg": color.ui.scrollHoverBgA,
+    "--pp-scrollbar-active-bg": color.ui.scrollActiveBgA,
+
+    "--pp-selection-bg": color.text.selectionBg,
+    "--pp-selection-overlay-bg": color.text.selectionBgA,
+    "--pp-selection-unfocused-bg": color.text.secondarySelectionBgA,
+
     "--pp-accent": color.accent.primary,
+    "--pp-accent-hover": color.accent.primaryHover,
+    "--pp-accent-secondary": color.accent.secondary,
+    "--pp-accent-secondary-hover": color.accent.secondaryHover,
+
+    "--pp-comment": syntax.lightPink,
     "--pp-link": color.accent.link,
     "--pp-link-hover": color.accent.linkHover,
 
-    "--pp-hint": color.diag.hint,
-    "--pp-info": color.diag.info,
-    "--pp-warning": color.diag.warning,
-    "--pp-error": color.diag.error,
-    "--pp-hint-bg": color.diag.hintBg,
-    "--pp-info-bg": color.diag.infoBg,
-    "--pp-warning-bg": color.diag.warningBg,
-    "--pp-error-bg": color.diag.errorBg,
+    "--pp-diag-hint": color.diag.hint,
+    "--pp-diag-info": color.diag.info,
+    "--pp-diag-warning": color.diag.warning,
+    "--pp-diag-error": color.diag.error,
+    "--pp-diag-hint-bg": color.diag.hintBg,
+    "--pp-diag-info-bg": color.diag.infoBg,
+    "--pp-diag-warning-bg": color.diag.warningBg,
+    "--pp-diag-error-bg": color.diag.errorBg,
 
     "--pp-git-added": color.git.addedOrStaged,
     "--pp-git-modified": color.git.modified,
@@ -50,6 +81,10 @@ function themeVars(color: UiColors, syntax: SyntaxColors, type: "light" | "dark"
     "--pp-git-untracked": color.git.untracked,
     "--pp-git-removed": color.git.removedOrConflicting,
     "--pp-git-ignored": color.git.ignoredOrSubmodule,
+    "--pp-git-inserted-bg": color.git.insertedBgA,
+    "--pp-git-removed-bg": color.git.removedBgA,
+    "--pp-git-current": color.git.current,
+    "--pp-git-incoming": color.git.incoming,
 
     "--pp-debug-start": color.debug.start,
     "--pp-debug-pause": color.debug.pause,
@@ -92,6 +127,14 @@ function themeVars(color: UiColors, syntax: SyntaxColors, type: "light" | "dark"
     "--pp-terminal-dim-blue": ansi.dimBlue,
     "--pp-terminal-dim-magenta": ansi.dimMagenta,
     "--pp-terminal-dim-cyan": ansi.dimCyan,
+
+    "--pp-chart-line": color.ui.chartLine,
+    "--pp-chart-blue": color.ui.chartBlue,
+    "--pp-chart-green": color.ui.chartGreen,
+    "--pp-chart-yellow": color.ui.chartYellow,
+    "--pp-chart-orange": color.ui.chartOrange,
+    "--pp-chart-red": color.ui.chartRed,
+    "--pp-chart-purple": color.ui.chartPurple,
   });
 
   return vars;
@@ -108,11 +151,13 @@ const dark = themeVars(darkColors, darkSyntax, "dark");
 const contrast = themeVars(blackContrastColors, blackContrastSyntax, "dark");
 const light = themeVars(lightColors, lightSyntax, "light");
 
-const css = `/* Prismatic Pink CSS properties */
+const css = `/* Prismatic Pink CSS properties
+ * Generated file. Do not edit manually.
+ */
 
 ${block(":root,\n.prismatic-pink-dark", dark)}
-${block(":root,\n.prismatic-pink-contrast", contrast)}
-${block(":root,\n.prismatic-pink-light", light)}
+${block(".prismatic-pink-contrast", contrast)}
+${block(".prismatic-pink-light", light)}
 `;
 
 const folder = makeOutputFolder("themes");
